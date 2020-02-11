225e4f9d36a3b1bc1677b9e07638c617f0b88309
Gicheha <geoffreygicheha@gmail.com>
2020-02-11 03:43:52 +0300

    Static file(s) management

--- finlance/settings.py
+++ finlance/settings.py
@@ -141,7 +141,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
