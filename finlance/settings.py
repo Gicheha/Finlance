0d2a700cdb86ffb91df5810d6d121b69e5ac8a00
Gicheha <geoffreygicheha@gmail.com>
2020-02-11 03:07:02 +0300

    static URL reversed

--- finlance/settings.py
+++ finlance/settings.py
@@ -140,8 +140,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-# STATIC_URL = '/static/'
-STATIC_URL = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+# STATIC_URL = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
